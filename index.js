306e203f03942b68df90b938133a2725c3bf92ad
4mrd <4mrddev@gmail.com>
2023-02-10 11:17:57 +0700

    add eslint

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 const fs = require('node:fs');
 const path = require('node:path');
 
-const { Client, Collection, Events, GatewayIntentBits } = require("discord.js");
+const { Client, Collection, Events, GatewayIntentBits } = require('discord.js');
 
 const { token } = require('./config.json');
 
 const client = new Client({
-    intents:[
-      GatewayIntentBits.Guilds
-    ],
+	intents:[
+		GatewayIntentBits.Guilds,
+	],
 });
 
 client.commands = new Collection();
@@ -23,7 +23,8 @@ for (const file of commandFiles) {
 	// Set a new item in the Collection with the key as the command name and the value as the exported module
 	if ('data' in command && 'execute' in command) {
 		client.commands.set(command.data.name, command);
-	} else {
+	}
+	else {
 		console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
 	}
 }
@@ -37,7 +38,8 @@ for (const file of eventFiles) {
 	const event = require(filePath);
 	if (event.once) {
 		client.once(event.name, (...args) => event.execute(...args));
-	} else {
+	}
+	else {
 		client.on(event.name, (...args) => event.execute(...args));
 	}
 }
@@ -51,19 +53,11 @@ client.on(Events.InteractionCreate, async interaction => {
 
 	try {
 		await command.execute(interaction);
-	} catch (error) {
+	}
+	catch (error) {
 		console.error(error);
 		await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
 	}
 });
-
-// client.on("ready", () =>{
-//     console.log("The AI bot is online"); //message when bot is online
-//   });
-// client.on("message", (message) => {
-//   if (message.content.substring(0, 1) === "!") {
-//     message.channel.send("Hello from AI bot"); //reply if message has "!" as first character
-//   }
-// });
 
 client.login(token);
